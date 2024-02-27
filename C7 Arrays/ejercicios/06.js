8059a1143d5f2ad39e197b8e779d81db954b3ddf
rodriguezMujica <gabrielrodriguez.germ@gmail.com>
2024-02-26 22:23:37 -0400

    ejercicios resueltos hasta el 8  del C7arrays

--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -2,7 +2,22 @@ function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
 
-  
+
+ 
+
+  array.reverse()
+
+
+  return(array)
+
+  // var  arrayInvertido = []
+
+  // for (let index = array.length; index <= 0; index++) {
+  //   array.
+    
+  // }
+
+
 }
 
 module.exports = invertirArray;
--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -2,6 +2,13 @@ function obtenerElementoAleatorio(array) {
    // Devuelve un elemento aleatorio del arreglo array.
    // PISTA: Usa el método Math.random().
    // Tu código:
+
+
+   var aleatorio = Math.random(1, array.length)
+
+ console.log(aleatorio)
+   return(array[aleatorio])
 }
+
 
 module.exports = obtenerElementoAleatorio;
